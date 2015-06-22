21536fc9c4dd39730744daaf3ec4cf3efc928e4a
Nicholas Emanuel <njemanuel01@gmail.com>
2015-06-22 14:25:28 -0500

    Added test for password checker.

--- user_test.rb
+++ user_test.rb
@@ -4,8 +4,17 @@ require_relative "category.rb"
 class CategoryTest < Minitest::Test
   #test the valid? method returns a correct boolean
   def test_valid?
-    hash = {"user_name" => "test_user"}
+    hash = {"user_name" => "test_user", "password" => "test_password"}
     test = User.new(hash)
     assert(test.valid?)
   end
+  
+  #test the valid_password method
+  def test_valid_password?
+    hash = {"user_name" => "test_user", "password" => "test_password"}
+    test = User.new(hash)
+    password = "test_password"
+    assert(test.valid_password?(password))
+  end
+  
 end
